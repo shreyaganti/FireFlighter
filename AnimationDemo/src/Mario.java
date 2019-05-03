7ada6f5f4b27d9b991e757fc6de0a8d67c6d002f
sganti910 <sganti910@PR12.hhs.win.fuhsd.org>
2019-05-03 10:14:07 -0700

    coded jump method

--- AnimationDemo/src/Mario.java
+++ AnimationDemo/src/Mario.java
@@ -31,7 +31,6 @@ public class Mario extends Sprite {
 	public void act(ArrayList<Shape> obstacles) {
 		super.moveByAmount(0, 0.5);
 		// FALL (and stop when a platform is hit)
-		moveByAmount(0,1);
 	}
 
 
