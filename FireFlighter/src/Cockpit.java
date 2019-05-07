c2762fb4ef9f55dbcca8b849b9b1ae472cebf49e
shreyag <shreyag@192.168.1.16>
2019-05-06 22:02:22 -0700

    Cockpit and dial classes act with arrow keys (temporary)

--- FireFlighter/src/Cockpit.java
+++ FireFlighter/src/Cockpit.java
@@ -1,10 +1,44 @@
+import processing.core.PApplet;
 
 public class Cockpit {
 
 	private Dial dial;
 	private double altitude;
 	
-	public Cockpit() {
-		dial = new Dial(0,0);
+	/**
+	 * Constructor to initialize Cockpit object with speed dial
+	 * @param d
+	 */
+	public Cockpit(Dial d) {
+		dial = d;
+	}
+	
+	/**
+	 * Draws Cockpit object with PApplet
+	 * @param p PApplet used to draw Cockpit object
+	 */
+	public void draw(PApplet p) {
+		dial.draw(p);
+		p.fill(255);
+		p.rect(25, 250, 150, 50);
+		p.fill(0);
+		p.textSize(26);
+		p.text(altitude+"", 80, 283);
+	}
+	
+	/**
+	 * Increments or decrements altitude of plane
+	 * @param a Value to change altitude by
+	 */
+	public void changeAltitude(double a) {
+		altitude+=a;
+	}
+	
+	/**
+	 * Returns altitude of plane
+	 * @return Altitude of plane
+	 */
+	public double getAltitude() {
+		return altitude;
 	}
 }
--- FireFlighter/src/DrawingSurface.java
+++ FireFlighter/src/DrawingSurface.java
@@ -3,12 +3,39 @@ import processing.core.PApplet;
 public class DrawingSurface extends PApplet {
 	
 	private Dial dial;
+	private Cockpit c;
 	
+	/**
+	 * Constructor to initial DrawingSurface object with a dial
+	 * @param d Dial to be added to DrawingSurface object
+	 */
 	public DrawingSurface(Dial d) {
 		dial = d;
+		c = new Cockpit(dial);
 	}
 	
+	/**
+	 * Draws components of DrawingSurface object
+	 */
 	public void draw() {
-		dial.draw(this);
+		c.draw(this);
+	}
+	
+	/**
+	 * Acts on detection of pressed keys
+	 */
+	public void keyPressed() {
+		if (keyCode == RIGHT && !dial.reachedMax()) {
+			dial.addSpeed(10);
+		}
+		else if (keyCode == LEFT && !dial.reachedMin()) {
+			dial.addSpeed(-10);
+		}
+		else if (keyCode == UP) {
+			c.changeAltitude(5);
+		}
+		else if (keyCode == DOWN) {
+			c.changeAltitude(-5);
+		}
 	}
 }
