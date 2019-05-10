70b7026d71786146426570fc7058c44fff127e54
asuriyaprakash978 <asuriyaprakash978@PR10.hhs.win.fuhsd.org>
2019-05-10 10:20:16 -0700

    Added shift method to runway

--- FireFlighter/src/Runway.java
+++ FireFlighter/src/Runway.java
@@ -1,12 +1,24 @@
 import processing.core.PApplet;
 import processing.core.PImage;
 
+/**
+ * This class represents a runway with an (x,y) coordinate
+ * 
+ * @author Ashwini Suriyaprakash
+ * @version 5/10/19
+ */
 public class Runway 
 {
 	private String airport;
 	private PImage runwayImage;
 	private double x,y;
 	
+	/**
+	 * Creates a Runway object
+	 * @param x x coordinate of runway
+	 * @param y y coordinate of runway
+	 * @param airport the airport the runway represents
+	 */
 	public Runway(double x, double y, String airport)
 	{
 		this.x = x;
@@ -24,8 +36,23 @@ public class Runway
 		runwayImage.resize(120, 60);
 	}
 	
+	/**
+	 * Draws the runway
+	 * @param drawer Papplet to draw the runway on
+	 */
 	public void draw(PApplet drawer)
 	{
 		drawer.image(runwayImage,(float)x, (float)y);
+	}
+	
+	/**
+	 * Shifts the runway by x and y amount
+	 * @param offsetX x amount to shift the runway by
+	 * @param offsetY y amount to shift the runway by
+	 */
+	public void shift(double offsetX, double offsetY)
+	{
+		x+=offsetX;
+		y+=offsetY;
 	}
 }
